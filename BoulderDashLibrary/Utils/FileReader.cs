fb13847dc7f451d3d7e88ce952bc26834a59fb67
= <basjansen14@outlook.com>
2018-01-10 12:47:33 +0100

    partly implemented the filereader

--- BoulderDashLibrary/Utils/FileReader.cs
+++ BoulderDashLibrary/Utils/FileReader.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BoulderDashLibrary.Utils
 {
-    public class FileReader
+    public static class FileReader
     {
+        public static string[] ReadFile()
+        {
+            try
+            {
+                var projectPath = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase)));
+                // Give correct path as parameter
+                var fullPath = new Uri(projectPath + @"\BoulderDash/....").LocalPath;
+
+                var textFile = File.ReadAllLines(fullPath);
+
+                return textFile;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Er is iets fout gegaan met het inladen van het bestand! \n" + e.InnerException);
+
+                return null;
+            }
+        }
     }
 }
