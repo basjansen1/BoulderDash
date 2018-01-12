fa762324e720c12be38fd940dfeffbf7a693958f
= <basjansen14@outlook.com>
2018-01-12 11:38:20 +0100

    fixes

--- BoulderDashLibrary/Model/Squares/AssetSquare.cs
+++ BoulderDashLibrary/Model/Squares/AssetSquare.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BoulderDashLibrary.Model.Square
+namespace BoulderDashLibrary.Model.Squares
 {
     public class AssetSquare
     {
---
+++ BoulderDashLibrary/Model/Squares/MudSquare.cs
@@ -0,0 +1,13 @@
+﻿using BoulderDashLibrary.Model.Squares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoulderDashLibrary.Model.Squares
+{
+    public class MudSquare : AssetSquare
+    {
+    }
+}
