c078f5f37b4bec02cb627d340eae39c9cae546dd
= <basjansen14@outlook.com>
2018-01-12 14:54:19 +0100

    updates

--- BoulderDashLibrary/Model/Replaceable.cs
+++ BoulderDashLibrary/Model/Replaceable.cs
@@ -10,15 +10,8 @@ namespace BoulderDashLibrary.Model
 {
     public abstract class Replaceable : PlayElement, IMovable
     {
-        private Square _square;
-        private string _name;
-        private Shapes _shape;
-
         public Replaceable(Square square, string name, Shapes shape) : base(square, name, shape)
         {
-            _square = square;
-            _name = name;
-            _shape = shape;
         }
 
         public bool CanMove(string direction)
@@ -26,19 +19,19 @@ namespace BoulderDashLibrary.Model
             switch (direction)
             {
                 case "Right":
-                    if (_square.RightSquare.PlayObject == null)
+                    if (CurrentSquare.RightSquare.PlayObject == null)
                         return true;
                     break;
                 case "Left":
-                    if (_square.LeftSquare.PlayObject == null)
+                    if (CurrentSquare.LeftSquare.PlayObject == null)
                         return true;
                     break;
                 case "Up":
-                    if (_square.UpSquare.PlayObject == null)
+                    if (CurrentSquare.UpSquare.PlayObject == null)
                         return true;
                     break;
                 case "Down":
-                    if (_square.DownSquare.PlayObject == null)
+                    if (CurrentSquare.DownSquare.PlayObject == null)
                         return true;
                     break;
                 default:
