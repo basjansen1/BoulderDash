0fa9b6dd404ead35572f79e8aab1a8ad82984257
= <basjansen14@outlook.com>
2018-01-14 15:28:12 +0100

    removed unused instance variable timer

--- BoulderDashLibrary/Controller/GameController.cs
+++ BoulderDashLibrary/Controller/GameController.cs
@@ -12,7 +12,6 @@ namespace BoulderDashLibrary.Controller
         #region fields
         private FieldController _fieldController;
         private ViewController _viewController;
-        private System.Timers.Timer _timer;
         private int _elapsedTime;
         #endregion
 
@@ -38,7 +37,7 @@ namespace BoulderDashLibrary.Controller
 
         private void StartTimer()
         {
-            System.Threading.Timer t = new System.Threading.Timer(TimerCallBack, null, 0, 1000);
+            var timer = new System.Threading.Timer(TimerCallBack, null, 0, 1000);
         }
 
         private void TimerCallBack(object o)
